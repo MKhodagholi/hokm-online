d32c046cddacb97189605da01955df6bfb61a7c3
MKhodagholi <MKhodaagholi@gmail.com>
2022-06-07 23:39:53 +0430

    3. add ul li and a style for Header Component

--- src/components/styles/HeaderStyled.js
+++ src/components/styles/HeaderStyled.js
@@ -1,6 +1,37 @@
 import styled from "styled-components";
 
 export const HeaderStyled = styled.header`
-  
-
+  ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    left: 50px;
+    top: 50px;
+    li {
+      margin-bottom: 20px;
+    }
+    a {
+      text-decoration: none;
+      color: ${({ theme }) => theme.colors.primary};
+      background-color: ${({ theme }) => theme.colors.secondary};
+      padding: 1rem;
+      width: 150px;
+      text-align: center;
+      display: inline-block;
+      border-radius: 5px;
+      font-size: 1.5rem;
+      font-family: "Lalezar";
+      box-shadow: inset 0 0 4px 4px rgba(0, 0, 0, 1);
+      border: 1px solid ${({ theme }) => theme.colors.primary};
+      transition: all 0.5s ease;
+      &:hover {
+        color: ${({ theme }) => theme.colors.secondary};
+        background-color: ${({ theme }) => theme.colors.primary};
+        border: 1px solid #fff;
+      }
+    }
+  }
 `;
