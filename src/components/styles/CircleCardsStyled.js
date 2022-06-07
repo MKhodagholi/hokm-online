0cff5260de30567708a4278fedeb779b60972955
MKhodagholi <MKhodaagholi@gmail.com>
2022-06-08 00:02:52 +0430

    5. Coding CircleCards Component with styled

---
+++ src/components/styles/CircleCardsStyled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const CircleCardsStyled = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: #000;
+  border-radius: 50%;
+  z-index: 2;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  border: 1px solid #fff;
+  transform: translate(-50%, -50%);
+`;
+
+export default CircleCardsStyled;
