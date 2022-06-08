ff514bb85098c7646b3ba9a33ecddabd95248ce3
MKhodagholi <MKhodaagholi@gmail.com>
2022-06-08 22:59:13 +0430

    6. add Card with prop in CardListStyled and coding CardListStyled css and add text[p]

--- src/components/CircleCards.jsx
+++ src/components/CircleCards.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import Card from "./Card";
+
 import CircleCardsStyled from "./styles/CircleCardsStyled";
+import { CardListStyled } from "./styles/CircleCardsStyled";
 
 const CircleCards = () => {
-  return <CircleCardsStyled></CircleCardsStyled>;
+  return (
+    <CircleCardsStyled>
+      <CardListStyled>
+        <Card rotateDegree={-15} height={400} width={300} />
+        <Card rotateDegree={5} height={400} width={300} />
+        <Card rotateDegree={15} height={400} width={300} />
+        <Card rotateDegree={25} height={400} width={300} />
+        <p>سلطان حکم</p>
+      </CardListStyled>
+    </CircleCardsStyled>
+  );
 };
 
 export default CircleCards;
--- src/components/styles/CircleCardsStyled.js
+++ src/components/styles/CircleCardsStyled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const CircleCardsStyled = styled.div`
+export const CircleCardsStyled = styled.div`
   width: 600px;
   height: 600px;
   background-color: #000;
@@ -11,6 +11,36 @@ const CircleCardsStyled = styled.div`
   top: 50%;
   border: 1px solid #fff;
   transform: translate(-50%, -50%);
+`;
+
+export const CardListStyled = styled.div`
+  display: flex;
+  margin-top: 25px;
+  margin-left: 80px;
+  div {
+    position: absolute;
+    &:nth-child(1) {
+      margin-left: 0;
+    }
+    &:nth-child(2) {
+      margin-left: 30px;
+    }
+    &:nth-child(3) {
+      margin-left: 60px;
+    }
+    &:nth-child(4) {
+      margin-left: 90px;
+    }
+  }
+  p {
+    position: fixed;
+    left: 50%;
+    bottom: 50px;
+    transform: translateX(-50%);
+    color: ${({ theme }) => theme.colors.secondary};
+    font-size: 3rem;
+    font-family: "Lalezar";
+  }
 `;
 
 export default CircleCardsStyled;
