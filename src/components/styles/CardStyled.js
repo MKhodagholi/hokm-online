cdbd529bc738e72186812ab06f1c6842a2f3c437
MKhodagholi <MKhodaagholi@gmail.com>
2022-06-12 01:23:26 +0430

    11. change border-radius from 10px to 12px and add overflow to hidden and add img styles

--- src/components/styles/CardStyled.js
+++ src/components/styles/CardStyled.js
@@ -4,16 +4,20 @@ const CardStyled = styled.div`
   width: ${({ width }) => width}px;
   height: ${({ height }) => height}px;
   background-color: #fff;
-  border-radius: 10px;
+  border-radius: 12px;
+  overflow: hidden;
   box-shadow: 0 0 4px 4px rgba(0, 0, 0, 0.5);
   transform: rotate(${({ rotateDegree }) => rotateDegree}deg);
   cursor: pointer;
   transition: all 0.5s ease;
+  img {
+    width: 100%;
+    height: 100%;
+  }
   &:hover {
     transform: rotate(${({ rotateDegree }) => -Math.abs(rotateDegree) - 5}deg)
-      translateX(-80px) !important;
+      translateX(-200px) !important;
     box-shadow: 0 0 4px 4px rgba(0, 0, 0, 0.8);
-    /* margin-left: -30px !important; */
   }
 `;
 
