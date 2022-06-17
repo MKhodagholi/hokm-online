b310505b20e3de1d3587dc0a50ec26459e0f037a
MKhodagholi <MKhodaagholi@gmail.com>
2022-06-17 13:57:28 +0430

    42. add app-options and restruct the profile component and make it to options view and change styles and delete the UserInfromationStyled

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,47 +1,71 @@
 import React from "react";
 import ProfileStyled from "./ProfileStyled";
 
-import UserInformationStyled from "./UserInformationStyled";
 import { AiOutlineUser, AiOutlinePhone, AiOutlineKey } from "react-icons/ai";
-import { MdOutlineMail } from "react-icons/md";
+import { MdOutlineMail, MdOutlinePrivacyTip } from "react-icons/md";
+import { SiMinutemailer } from "react-icons/si";
+import { IoHelpOutline } from "react-icons/io5";
 
 const Profile = () => {
   return (
     <ProfileStyled>
-      <UserInformationStyled>
-        <div className="user-information user-name">
+      <div className="options user">
+        <div className="option user-name">
           <AiOutlineUser className="user-name__icon" />
-          <div className="title-description">
+          <div className="option-title">
             <label>نام کاربری </label>
             <span>محمد خداقلی</span>
           </div>
-          <div className="user-informaion__go">{">"}</div>
+          <div className="option-go">{">"}</div>
         </div>
-        <div className="user-information user-password">
-          <AiOutlineKey className="user-password__icon" />
-          <div className="title-description">
+        <div className="option user-password">
+          <AiOutlineKey className="option-icon" />
+          <div className="option-title">
             <label>پسورد </label>
             <span>آخرین تغییر ۲ سال پیش</span>
           </div>
-          <div className="user-informaion__go">{">"}</div>
+          <div className="option-go">{">"}</div>
         </div>
-        <div className="user-information user-email">
-          <MdOutlineMail className="user-email__icon" />
-          <div className="title-description">
+        <div className="option user-email">
+          <MdOutlineMail className="option-icon" />
+          <div className="option-title">
             <label>ایمیل </label>
             <span>mdkhoda****@gmail.com</span>
           </div>
-          <div className="user-informaion__go">{">"}</div>
+          <div className="option-go">{">"}</div>
         </div>
-        <div className="user-information user-phone">
-          <AiOutlinePhone className="user-phone__icon" />
-          <div className="title-description">
+        <div className="option user-phone">
+          <AiOutlinePhone className="option-icon" />
+          <div className="option-title">
             <label>موبایل </label>
             <span dir="ltr">+۹۸ ۹۹۰ ۹۲۱ ۹۸۳۵ </span>
           </div>
-          <div className="user-informaion__go">{">"}</div>
+          <div className="option-go">{">"}</div>
         </div>
-      </UserInformationStyled>
+      </div>
+      <div className="options app">
+        <div className="option tutorial">
+          <IoHelpOutline className="option-icon" />
+          <div className="option-title">
+            <label>آموزش </label>
+          </div>
+          <div className="option-go">{">"}</div>
+        </div>
+        <div className="option privacy">
+          <MdOutlinePrivacyTip className="option-icon" />
+          <div className="option-title">
+            <label>حریم خصوصی و شرایط </label>
+          </div>
+          <div className="option-go">{">"}</div>
+        </div>
+        <div className="option privacy">
+          <SiMinutemailer className="option-icon" />
+          <div className="option-title">
+            <label>ارتباط با ما </label>
+          </div>
+          <div className="option-go">{">"}</div>
+        </div>
+      </div>
     </ProfileStyled>
   );
 };
--- src/pages/Profile/ProfileStyled.js
+++ src/pages/Profile/ProfileStyled.js
@@ -7,6 +7,50 @@ const ProfileStyled = styled.div`
   direction: rtl;
   color: #fff;
   font-family: "Lalezar";
+  .options {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    align-items: center;
+    width: 90%;
+    margin: 1rem auto;
+    border-radius: 10px;
+    background-color: ${({ theme }) => theme.colors.darkBlueColor};
+    color: ${({ theme }) => theme.colors.whiteColor};
+    .option {
+      display: flex;
+      align-items: center;
+      padding-top: 1rem;
+      font-size: 1.5rem;
+      width: 100%;
+      svg {
+        margin: 0 1rem;
+        margin-left: 0;
+        margin-left: 10px;
+        padding: 0.5rem;
+        font-size: 2.5rem;
+        border: 2px solid ${({ theme }) => theme.colors.primary};
+        border-radius: 50%;
+      }
+      .option-title {
+        display: flex;
+        flex-direction: column;
+        span {
+          font-size: 0.75rem;
+          color: ${({ theme }) => theme.colors.grayColor};
+        }
+      }
+      .option-go {
+        position: absolute;
+        left: 30px;
+        color: ${({ theme }) => theme.colors.grayColor};
+        cursor: pointer;
+      }
+      &:last-child {
+        padding-bottom: 1rem;
+      }
+    }
+  }
 `;
 
 export default ProfileStyled;
