12066051baed3980b1b031a889e67f51fa13e615
MKhodagholi <MKhodaagholi@gmail.com>
2022-08-07 13:54:12 +0430

    44. add overflow: hidden to App Component

--- src/components/styles/AppStyled.js
+++ src/components/styles/AppStyled.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const AppStyled = styled.div`
   min-height: 100vh;
   max-width: 500px;
+  overflow: hidden;
   margin: auto;
   background-color: ${(props) => props.theme.colors.primary};
 `;
